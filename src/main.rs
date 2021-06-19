f064d23ab4438e551b65561a63ecb54241c7eb98
Lee Martin <lee.martin@pliminus.co.uk>
2021-06-19 23:05:27 +0100

    Update: Breaking that big monolith of a system into some smaller ones

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,9 @@ fn main() {
         .insert_resource(ClearColor(Color::rgb(0.9, 0.9, 0.9)))
         .add_startup_system(setup.system())
         .add_system(player_input_system.system())
-        .add_system(movable_object_update_system.system())
+        .add_system(thrust_system.system())
+        .add_system(gravity_system.system())
+        .add_system(velocity_system.system())
         .run();
 }
 
@@ -109,16 +111,18 @@ fn player_input_system(
     }
 }
 
-fn movable_object_update_system(
+fn gravity_system(
     time: Res<Time>,
     mut set: QuerySet<(
         Query<(&Gravity, &Transform, &Planet)>,
-        Query<(&Gravity, &Thrust, &mut Transform, &mut Velocity)>
+        Query<(&Gravity, &Transform, &mut Velocity)>
     )>
 ) {
     let mut planet_mass_radius = MassRadius { mass: 0.0, radius: 0.0 };
     let mut planet_translation = Vec3::new(0.0, 0.0, 0.0);
     if let Ok((planet_gravity, planet_transform, _planet)) = set.q0().single() {
+        //Some cheating is going on here:
+        // - Only one planet
         if let Gravity::Immovable(pmr) = planet_gravity {
             planet_mass_radius.mass = pmr.mass;
             planet_mass_radius.radius = pmr.radius;
@@ -126,37 +130,53 @@ fn movable_object_update_system(
             planet_translation.y = planet_transform.translation.y;
         }
     }
-    if let Ok((player_gravity, thrust, mut player_transform, mut velocity)) = set.q1_mut().single_mut() {
-        if let Gravity::Movable(player_mass_radius) = player_gravity {
-            //Some cheating is going on here:
-            // - Only one planet
-            // - Only one player
-            // - Working on the assumption of the "gravity" type
-            let distance_between_objects = distance_between_two_vec(planet_translation, player_transform.translation);
-            if distance_between_objects <= (planet_mass_radius.radius + player_mass_radius.radius) {
-                // player has crashed - time to restart;
-                // Teeeechnically we should probably call this updating - but it will get hit after anyway
-                return;
+    for (object_gravity, object_transform, mut velocity) in set.q1_mut().iter_mut() {
+        if let Gravity::Movable(object_mass_radius) = object_gravity {
+            let distance_between_objects = distance_between_two_vec(planet_translation, object_transform.translation);
+            if distance_between_objects <= (planet_mass_radius.radius + object_mass_radius.radius) {
+                // object has crashed - it needs to stop moving
+                velocity.velocity.x = 0.0;
+                velocity.velocity.y = 0.0;
+                velocity.velocity.z = 0.0;
+                continue;
             }
             //We should definitely not be calcing this constantly.
             //https://en.wikipedia.org/wiki/Gravitational_constant
             let gravitational_constant: f32 = 6.674f32 * (10f32).powi(-11);
-            let force = gravitational_constant * ((player_mass_radius.mass * planet_mass_radius.mass) / distance_between_objects.powi(2));
+            let force = gravitational_constant * ((object_mass_radius.mass * planet_mass_radius.mass) / distance_between_objects.powi(2));
             //F=MA
             //A = F/M
-            let acceleration = force / player_mass_radius.mass;
+            let acceleration = force / object_mass_radius.mass;
             //A = distance/time
             //distance = A*time
             let distance = acceleration * time.delta_seconds();
-            let normalised_vector = vec_from_angle(angle_between_two_vec(planet_translation, player_transform.translation));
+            let normalised_vector = vec_from_angle(angle_between_two_vec(planet_translation, object_transform.translation));
             velocity.velocity += normalised_vector * distance;
-            if thrust.thrust > 0.0 {
-                velocity.velocity += time.delta_seconds() * (thrust.thrust * vec_from_angle(thrust.facing));
-            }
-            player_transform.translation += time.delta_seconds() * velocity.velocity;
         }
     }
 }
+
+fn thrust_system(
+    time: Res<Time>,
+    mut query: Query<(&Thrust, &mut Velocity)>
+) {
+    for (thrust, mut velocity) in query.iter_mut() {
+        if thrust.thrust > 0.0 {
+            velocity.velocity += time.delta_seconds() * (thrust.thrust * vec_from_angle(thrust.facing));
+        }
+    }
+}
+
+fn velocity_system(
+    time: Res<Time>,
+    mut query: Query<(&mut Transform, &Velocity)>
+) {
+    for (mut transform, velocity) in query.iter_mut() {
+        transform.translation += time.delta_seconds() * velocity.velocity;
+    }
+}
+
+
 
 //Note: this all uses Vec3s, we only want to care about x and y though
 
