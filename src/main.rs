a483f200831562fab2cb3166ce28447bf956380b
Lee Martin <lee.martin@pliminus.co.uk>
2021-06-19 22:45:12 +0100

    Update: Breaking as much out to reusable components as possible

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
         .insert_resource(ClearColor(Color::rgb(0.9, 0.9, 0.9)))
         .add_startup_system(setup.system())
         .add_system(player_input_system.system())
-        .add_system(player_update_system.system())
+        .add_system(movable_object_update_system.system())
         .run();
 }
 
@@ -25,10 +25,13 @@ fn setup(
             sprite: Sprite::new(Vec2::new(10.0, 10.0)),
             ..Default::default()
         })
-        .insert(Player {
-            velocity: Vec3::new(150.0, 0.0, 0.0),
+        .insert(Player {})
+        .insert(Thrust {
             thrust: 0.0,
             facing: 0.0,
+        })
+        .insert(Velocity {
+            velocity: Vec3::new(150.0, 0.0, 0.0),
         })
         .insert(Gravity::Movable(MassRadius {
             radius: 10.0,
@@ -54,15 +57,21 @@ struct MassRadius {
     radius: f32
 }
 
+struct Velocity {
+    velocity: Vec3
+}
+
+struct Thrust {
+    thrust: f32,
+    facing: f32
+}
+
 enum Gravity {
     Movable(MassRadius),
     Immovable(MassRadius)
 }
 
 struct Player {
-    velocity: Vec3,
-    thrust: f32,
-    facing: f32,
 }
 
 
@@ -75,41 +84,41 @@ const PLAYER_TURN_RATE: f32 = 10.0;
 fn player_input_system(
     time: Res<Time>,
     keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<(&mut Player, &mut Transform)>,
+    mut query: Query<(&Player, &mut Thrust)>,
 ) {
-    if let Ok((mut player, mut _transform)) = query.single_mut() {
+    if let Ok((_player, mut thrust)) = query.single_mut() {
         if keyboard_input.pressed(KeyCode::Left) {
-            player.facing -= PLAYER_TURN_RATE * time.delta_seconds();
+            thrust.facing -= PLAYER_TURN_RATE * time.delta_seconds();
         }
 
         if keyboard_input.pressed(KeyCode::Right) {
-            player.facing += PLAYER_TURN_RATE * time.delta_seconds();
+            thrust.facing += PLAYER_TURN_RATE * time.delta_seconds();
         }
 
         if keyboard_input.pressed(KeyCode::Up) {
-            player.thrust += PLAYER_ACCELERATION_RATE * time.delta_seconds();
+            thrust.thrust += PLAYER_ACCELERATION_RATE * time.delta_seconds();
         }
 
         if keyboard_input.pressed(KeyCode::Down) {
-            player.thrust -= PLAYER_ACCELERATION_RATE * time.delta_seconds();
+            thrust.thrust -= PLAYER_ACCELERATION_RATE * time.delta_seconds();
             
         }
-        if player.thrust < 0.0 {
-            player.thrust = 0.0;
+        if thrust.thrust < 0.0 {
+            thrust.thrust = 0.0;
         }
     }
 }
 
-fn player_update_system(
+fn movable_object_update_system(
     time: Res<Time>,
     mut set: QuerySet<(
-        Query<(&Gravity, &mut Transform, &mut Player)>,
-        Query<(&Gravity, &Transform, &Planet)>
+        Query<(&Gravity, &Transform, &Planet)>,
+        Query<(&Gravity, &Thrust, &mut Transform, &mut Velocity)>
     )>
 ) {
     let mut planet_mass_radius = MassRadius { mass: 0.0, radius: 0.0 };
     let mut planet_translation = Vec3::new(0.0, 0.0, 0.0);
-    if let Ok((planet_gravity, planet_transform, _planet)) = set.q1_mut().single() {
+    if let Ok((planet_gravity, planet_transform, _planet)) = set.q0().single() {
         if let Gravity::Immovable(pmr) = planet_gravity {
             planet_mass_radius.mass = pmr.mass;
             planet_mass_radius.radius = pmr.radius;
@@ -117,7 +126,7 @@ fn player_update_system(
             planet_translation.y = planet_transform.translation.y;
         }
     }
-    if let Ok((player_gravity, mut player_transform, mut player)) = set.q0_mut().single_mut() {
+    if let Ok((player_gravity, thrust, mut player_transform, mut velocity)) = set.q1_mut().single_mut() {
         if let Gravity::Movable(player_mass_radius) = player_gravity {
             //Some cheating is going on here:
             // - Only one planet
@@ -140,13 +149,11 @@ fn player_update_system(
             //distance = A*time
             let distance = acceleration * time.delta_seconds();
             let normalised_vector = vec_from_angle(angle_between_two_vec(planet_translation, player_transform.translation));
-            player.velocity += normalised_vector * distance;
-            if player.thrust > 0.0 {
-                let thrust = player.thrust;
-                let facing = player.facing;
-                player.velocity += time.delta_seconds() * (thrust * vec_from_angle(facing));
+            velocity.velocity += normalised_vector * distance;
+            if thrust.thrust > 0.0 {
+                velocity.velocity += time.delta_seconds() * (thrust.thrust * vec_from_angle(thrust.facing));
             }
-            player_transform.translation += time.delta_seconds() * player.velocity;
+            player_transform.translation += time.delta_seconds() * velocity.velocity;
         }
     }
 }
