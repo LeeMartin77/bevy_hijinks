e6007516edb34283525aee4e0fc3cc197d3af450
Lee Martin <lee.martin@pliminus.co.uk>
2021-06-21 21:38:49 +0100

    Update: Switching from actual 3D being ortho rendered, to a vector/lyon based setup for graphics

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod components;
 mod systems;
 
 use bevy::prelude::*;
+use bevy_prototype_lyon::prelude::*;
 
 use crate::components::*;
 use crate::systems::*;
@@ -9,6 +10,7 @@ use crate::systems::*;
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
+        .add_plugin(ShapePlugin)
         .insert_resource(ClearColor(Color::rgb(0.7, 0.7, 0.7)))
         .add_startup_system(setup::setup.system())
         .add_system(input::player_input_system.system())
--- src/systems/setup.rs
+++ src/systems/setup.rs
@@ -1,28 +1,27 @@
 use bevy::prelude::*;
+use bevy_prototype_lyon::prelude::*;
 
 use crate::components::*;
 
 pub fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut commands: Commands
 ) {
+    //I'm 100% sure this can be gotten in data, but lets take the path of least resistance right now
     let player_radius = 1.0;
+
+    let player_circle = shapes::Circle {
+        radius: player_radius,
+        ..shapes::Circle::default()
+    };
     commands
-        .spawn_bundle(PbrBundle {
-            material: materials.add(StandardMaterial { 
-                base_color: Color::rgb(1.0, 0.0, 0.0).into(),
-                metallic: 0.9,
-                roughness: 0.3,
-                ..Default::default()
-            }),
-            transform: Transform::from_xyz(0.0, 15.0, 0.0),
-            mesh: meshes.add(Mesh::from(shape::Icosphere { 
-                radius: player_radius, 
-                subdivisions: 32, 
-            })),
-            ..Default::default()
-        })
+        .spawn_bundle(
+            GeometryBuilder::build_as(
+                &player_circle,
+                ShapeColors::new(Color::TEAL),
+                DrawMode::Fill(FillOptions::default()),
+                Transform::from_xyz(0.0, 15.0, 0.0),
+            )
+        )
         .insert(entities::Player { })
         .insert(physical_attributes::Thrust {
             thrust: 0.0,
@@ -37,41 +36,29 @@ pub fn setup(
         }));
         
     let planet_radius = 10.0;
+
+    let planet_circle = shapes::Circle {
+        radius: planet_radius,
+        ..shapes::Circle::default()
+    };
     commands
-        .spawn_bundle(PbrBundle {
-            material: materials.add(StandardMaterial { 
-                base_color: Color::rgb(0.3, 0.3, 0.3).into(),
-                metallic: 0.1,
-                roughness: 0.7,
-                ..Default::default()
-            }),
-            transform: Transform::from_xyz(0.0, 0.0, 0.0),
-            mesh: meshes.add(Mesh::from(shape::Icosphere { 
-                radius: planet_radius, 
-                subdivisions: 32, 
-            })),
-            ..Default::default()
-        })
+        .spawn_bundle(
+            GeometryBuilder::build_as(
+                &planet_circle,
+                ShapeColors::new(Color::BLACK),
+                DrawMode::Fill(FillOptions::default()),
+                Transform::from_xyz(0.0, 0.0, 0.0),
+            )
+        )
         .insert(entities::Planet { })
         .insert(physical_attributes::Gravity::Immovable(physical_attributes::MassRadius {
             radius: planet_radius,
             mass: 5.0 * (10f32).powi(13)
         }));
 
-    let mut camera = OrthographicCameraBundle::new_3d();
-    camera.orthographic_projection.scale = 50.0;
-    camera.transform = Transform::from_xyz(0.0, 0.0, 25.0).looking_at(Vec3::ZERO, Vec3::Y);
+    let mut camera = OrthographicCameraBundle::new_2d();
+    camera.orthographic_projection.scale = 0.1;
+    camera.transform = Transform::from_xyz(0.0, 0.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y);
 
     commands.spawn_bundle(camera);
-    
-    commands.spawn_bundle(LightBundle {
-        light: Light {
-            range: 200.0,
-            intensity: 2000.0,
-            fov: f32::to_radians(360.0),
-            ..Default::default()
-        },
-        transform: Transform::from_xyz(-50.0, 40.0, 20.0),
-        ..Default::default()
-    });
 }
