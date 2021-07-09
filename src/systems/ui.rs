284c1d1ccd3c95e006080f6806e2637951cc6ee3
Lee Martin <lee.martin@pliminus.co.uk>
2021-07-09 17:03:42 +0100

    Added: A bit more UI for the player

--- src/systems/ui.rs
+++ src/systems/ui.rs
@@ -1,9 +1,12 @@
+use crate::components::entities::Player;
+use crate::components::physical_attributes::Thrust;
 use bevy::prelude::*;
 
 use crate::components::resources::*;
 
 pub enum UiTextElements {
-    RestartText
+    RestartText,
+    ControlData
 }
 
 
@@ -11,13 +14,29 @@ pub fn ui_system(
     mut _commands: Commands,
     _time: Res<Time>,
     gamestate: ResMut<GameState>,
-    mut query: Query<(&UiTextElements, &mut Visible)>,
+    mut set: QuerySet<(
+        Query<(&UiTextElements, &mut Visible, &mut Text)>,
+        Query<(&Player, &Thrust)>
+    )>
 ) {
-    for (text_element, visible) in query.iter_mut() {
+    let mut player_thrust = Thrust { facing: 0.0, thrust: 0.0 };
+
+    if let Ok((_player, thrust)) = set.q1().single() {
+        player_thrust.facing = thrust.facing;
+        player_thrust.thrust = thrust.thrust;
+    }
+
+    for (text_element, visible, text) in set.q0_mut().iter_mut() {
         match text_element {
-            UiTextElements::RestartText => update_restart_text(&gamestate, visible)
+            UiTextElements::RestartText => update_restart_text(&gamestate, visible),
+            UiTextElements::ControlData => update_control_text(text, &player_thrust)
         }
     }
+}
+
+fn update_control_text(mut text: Mut<Text>, thrust: &Thrust){
+    text.sections[0].value = THRUST_LABEL.to_string() + &thrust.thrust.to_string() + "\n";
+    text.sections[1].value = FACING_LABEL.to_string() + &thrust.facing.to_string();
 }
 
 fn update_restart_text(gamestate: &ResMut<GameState>, mut visible: Mut<Visible>){
@@ -32,6 +51,9 @@ fn update_restart_text(gamestate: &ResMut<GameState>, mut visible: Mut<Visible>)
 pub trait UiExtensions {
     fn add_ui_elements(&mut self, asset_server: Res<AssetServer>);
 }
+
+const THRUST_LABEL: &str = "Thrust: ";
+const FACING_LABEL: &str = "Facing: ";
 
 impl UiExtensions for Commands<'_> {
     fn add_ui_elements(&mut self, asset_server: Res<AssetServer>) {
@@ -65,5 +87,45 @@ impl UiExtensions for Commands<'_> {
                 ..Default::default()
             })
             .insert(UiTextElements::RestartText);
+
+            self.spawn_bundle(TextBundle {
+                style: Style {
+                    align_self: AlignSelf::FlexStart,
+                    position_type: PositionType::Absolute,
+                    position: Rect {
+                        top: Val::Px(5.0),
+                        left: Val::Px(15.0),
+                        ..Default::default()
+                    },
+                    ..Default::default()
+                },
+                text: Text{
+                    sections: vec![
+                        TextSection {
+                            value: THRUST_LABEL.to_string() + "\n",
+                            style: TextStyle {
+                                font: asset_server.load("fonts/ShareTechMono-Regular.ttf"),
+                                font_size: 20.0,
+                                color: Color::WHITE,
+                            }
+                        },
+                        TextSection {
+                            value: FACING_LABEL.to_string(),
+                            style: TextStyle {
+                                font: asset_server.load("fonts/ShareTechMono-Regular.ttf"),
+                                font_size: 20.0,
+                                color: Color::WHITE,
+                            }
+                        },
+                    ],
+                    alignment: TextAlignment {
+                        horizontal: HorizontalAlign::Left,
+                        vertical: VerticalAlign::Bottom
+                    },
+                },
+                visible: Visible::default(),
+                ..Default::default()
+            })
+            .insert(UiTextElements::ControlData);
     }
 }
